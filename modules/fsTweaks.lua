80986cbe3e3745a1225eaaa9e228a75760098443
jSteel <jacobsteel9@gmail.com>
2025-09-11 13:38:00 +0100

    Create fsTweaks.lua v0.1.0
    Patches the fs module while maintaining an original copy in the global environment.
    Currently just adds a function to get the extension of a file but supports full monkey patch capability

---
+++ modules/fsTweaks.lua
@@ -0,0 +1,35 @@
+-- Initialise global table to track patched modules and store the original modules for redundancy
+if not _G.patched then _G.patched = {original = {}, patched = {}} end
+_G.patched.original["fs"] = fs -- Store original fs module
+
+
+fsTweaks = fs
+fsTweaks.semver = "0.1.0" -- https://semver.org/
+--[[
+    Load FS Tweaks Functions into Environment
+]]
+---@comment Get the file extension from a given path
+---@param path path
+---@return string?
+fsTweaks.getExtension = function(path)
+    return path:match("^.+(%..+)$")
+end
+
+-- Store patched fs module
+_G.patched.patched["fs"] = fsTweaks
+
+--[[
+    Apply Patches to Global FS Module
+]]
+setMetaTable(fs, {
+    __index = function(t, k)
+        if fsTweaks[k] then
+            return fsTweaks[k]
+        else
+            return _G.patched.original["fs"][k]
+        end
+    end
+})
+
+-- Verify Patch Applied and Return Value
+return fsTweaks == fs and true or false
