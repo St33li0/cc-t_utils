98d0c5305122605e355b1145c78b0c332e0e49f7
St33li0 <jacobsteel9@gmail.com>
2026-02-10 07:00:48 +0000

    Add screenBuffer module for managing terminal output

---
+++ bin/botnet/host/screenBuffer.lua
@@ -0,0 +1,48 @@
+local screenBuffer = {}
+screenBuffer.__index = screenBuffer
+
+local function centeredPrint(tx,text)
+    local x = math.floor((tx - #text) / 2) + 1
+    local _,y = term.getCursorPos()
+    term.setCursorPos(x, y)
+    write(text.."\n")
+end
+
+function screenBuffer.new(args)
+    local self = setmetatable({}, screenBuffer)
+    self.titleBar = args.titleBar or ""
+    self.lines = args.lines or {}
+    self.tx,self.ty = term.getSize()
+    return self
+end
+
+function screenBuffer:draw()
+    term.clear()
+    term.setCursorPos(1,1)
+    centeredPrint(self.tx, self.titleBar)
+    for i, drawFunc in ipairs(self.lines) do
+        term.setCursorPos(1, i + 1)
+        drawFunc()
+    end
+end
+
+function screenBuffer:add(text)
+    table.insert(self.lines, function() write(text.."\n" or "\n") end)
+end
+
+function screenBuffer:addCentered(text)
+    table.insert(self.lines, function() centeredPrint(self.tx,text or "") end)
+end
+
+function screenBuffer:clear()
+    self.lines = {}
+    self:draw()
+end
+
+function screenBuffer:setTitle(title)
+    if title ~= "" then
+        self.titleBar = title
+    end
+end
+
+return screenBuffer
